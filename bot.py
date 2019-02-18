89f4115e8e3c485753a7cdbec8b8a41ae3d8d0d7
Winston <winston@Winstons-MacBook-Pro.local>
2019-02-18 03:40:38 -0800

    cleaning

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
+from time import sleep
 import random
 import sys
 
@@ -27,10 +27,10 @@ class InstagramBot:
     def login(self):
         driver = self.driver
         driver.get("https://www.instagram.com/")
-        time.sleep(2)
+        sleep(2)
         login_button = driver.find_element_by_xpath("//a[@href='/accounts/login/?source=auth_switcher']")
         login_button.click()
-        time.sleep(2)
+        sleep(2)
         user_name_elem = driver.find_element_by_xpath("//input[@name='username']")
         user_name_elem.clear()
         user_name_elem.send_keys(self.username)
@@ -38,19 +38,19 @@ class InstagramBot:
         passworword_elem.clear()
         passworword_elem.send_keys(self.password)
         passworword_elem.send_keys(Keys.RETURN)
-        time.sleep(2)
+        sleep(2)
 
     def like_photo(self, hashtag):
         driver = self.driver
         driver.get("https://www.instagram.com/explore/tags/" + hashtag + "/")
-        time.sleep(2)
+        sleep(2)
 
         # gathering photos
         pic_hrefs = []
-        for i in range(7):
+        for _ in range(7):
             try:
                 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                time.sleep(2)
+                sleep(2)
                 # get tags
                 hrefs_in_view = driver.find_elements_by_tag_name('a')
                 # finding relevant hrefs
@@ -59,39 +59,40 @@ class InstagramBot:
                 # building list of unique photos
                 [pic_hrefs.append(href) for href in hrefs_in_view if href not in pic_hrefs]
                 # print("Check: pic href length " + str(len(pic_hrefs)))
-            except Exception:
+            except:
                 print('except Exception: # gathering photos')
                 continue
 
-        # Liking photos
+        # liking photos
         unique_photos = len(pic_hrefs)
         for pic_href in pic_hrefs:
             driver.get(pic_href)
-            time.sleep(2)
+            sleep(2)
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             try:
-                time.sleep(random.randint(2, 4))
+                sleep(random.randint(2, 4))
                 like_button = lambda: driver.find_element_by_xpath('//span[@aria-label="Like"]').click()
                 like_button().click()
                 for second in reversed(range(0, random.randint(18, 28))):
                     print_same_line("#" + hashtag + ': unique photos left: ' + str(unique_photos)
                                     + " | Sleeping " + str(second))
-                    time.sleep(1)
-            except Exception as e:
-                time.sleep(2)
+                    sleep(1)
+            except:
+                sleep(2)
             unique_photos -= 1
 
 if __name__ == "__main__":
 
-    username = ''
-    password = ''
+    username = ''  # your username 
+    password = ''  # your password
 
     ig = InstagramBot(username, password)
     ig.login()
 
-    hashtags = ['codnation', 'callofduty', 'cwl', 'twitch', 'blackout',
-                'killcam', 'codclips', 'blackops', 'freeforall', 'twitchclips', 'blackoutgame', 'xbox',
-                'battleroyale', 'ps4', 'gamingmemes', 'bo4', 'blackops4', 'callofdutyblackops4']
+    # insert your desired hashtags here (list)
+    hashtags = ['codnation', 'callofduty', 'cwl', 'twitch', 'blackout', 'killcam', 'codclips', 
+                'blackops', 'freeforall', 'twitchclips', 'blackoutgame', 'xbox', 'ps4',
+                'battleroyale', 'gamingmemes', 'bo4', 'blackops4', 'callofdutyblackops4']
 
     while True:
         try:
@@ -100,6 +101,6 @@ if __name__ == "__main__":
             ig.like_photo(tag)
         except Exception:
             ig.closeBrowser()
-            time.sleep(60)
+            sleep(61)
             ig = InstagramBot(username, password)
-ig.login()
+# ig.login()
