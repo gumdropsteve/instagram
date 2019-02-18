d975b2e1a30faea6ae04167da66f7a1ae167449b
Winston <winston@Winstons-MacBook-Pro.local>
2019-02-18 03:55:17 -0800

    sorting for clarity

--- bot.py
+++ bot.py
@@ -4,6 +4,18 @@ from time import sleep
 import random
 import sys
 
+# plugins, base urls, and paths
+instagram = 'https://www.instagram.com/'
+ig_login_button = '//a[@href="/accounts/login/?source=auth_switcher"]'
+username_box = '//input[@name="username"]'
+password_box = '//input[@name="password"]'
+instagram_tags_url = 'https://www.instagram.com/explore/tags/'
+scroll = "window.scrollTo(0, document.body.scrollHeight);"
+like = '//span[@aria-label="Like"]'
+tags = ['codnation', 'callofduty', 'cwl', 'twitch', 'blackout', 'killcam', 'codclips', 
+                'blackops', 'freeforall', 'twitchclips', 'blackoutgame', 'xbox', 'ps4',
+                'battleroyale', 'gamingmemes', 'bo4', 'blackops4', 'callofdutyblackops4']
+
 
 def print_same_line(text):
     sys.stdout.write('\r')
@@ -15,8 +27,14 @@ def print_same_line(text):
 class InstagramBot:
 
     def __init__(self, username, password):
-        options = webdriver.FirefoxOptions()  # (seems to) initiate(s) webdriver.Firefox() once referenced 
-        options.set_preference("dom.push.enabled", False)  # blocks popups 
+        options = webdriver.FirefoxOptions()  
+        options.set_preference("dom.push.enabled", False)  
+        '''
+        options allows the blocking of popups 
+        this is not required for this script as it is currently written
+        may become necessary in future editions
+        to disable, change (options=options) to ()
+        '''
         self.username = username
         self.password = password
         self.driver = webdriver.Firefox(options=options)
@@ -26,15 +44,15 @@ class InstagramBot:
 
     def login(self):
         driver = self.driver
-        driver.get("https://www.instagram.com/")
+        driver.get(instagram)
         sleep(2)
-        login_button = driver.find_element_by_xpath("//a[@href='/accounts/login/?source=auth_switcher']")
+        login_button = driver.find_element_by_xpath(ig_login_button)
         login_button.click()
         sleep(2)
-        user_name_elem = driver.find_element_by_xpath("//input[@name='username']")
+        user_name_elem = driver.find_element_by_xpath(username_box)
         user_name_elem.clear()
         user_name_elem.send_keys(self.username)
-        passworword_elem = driver.find_element_by_xpath("//input[@name='password']")
+        passworword_elem = driver.find_element_by_xpath(password_box)
         passworword_elem.clear()
         passworword_elem.send_keys(self.password)
         passworword_elem.send_keys(Keys.RETURN)
@@ -42,14 +60,14 @@ class InstagramBot:
 
     def like_photo(self, hashtag):
         driver = self.driver
-        driver.get("https://www.instagram.com/explore/tags/" + hashtag + "/")
+        driver.get(instagram_tags_url + hashtag + '/')
         sleep(2)
 
         # gathering photos
         pic_hrefs = []
         for _ in range(7):
             try:
-                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                driver.execute_script(scroll)
                 sleep(2)
                 # get tags
                 hrefs_in_view = driver.find_elements_by_tag_name('a')
@@ -68,10 +86,10 @@ class InstagramBot:
         for pic_href in pic_hrefs:
             driver.get(pic_href)
             sleep(2)
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            driver.execute_script(scroll)
             try:
                 sleep(random.randint(2, 4))
-                like_button = lambda: driver.find_element_by_xpath('//span[@aria-label="Like"]').click()
+                like_button = lambda: driver.find_element_by_xpath(like).click()
                 like_button().click()
                 for second in reversed(range(0, random.randint(18, 28))):
                     print_same_line("#" + hashtag + ': unique photos left: ' + str(unique_photos)
@@ -83,16 +101,14 @@ class InstagramBot:
 
 if __name__ == "__main__":
 
-    username = ''  # your username 
-    password = ''  # your password
+    user_id = ''  # your username 
+    pword = ''  # your password
 
-    ig = InstagramBot(username, password)
+    ig = InstagramBot(user_id, pword)
     ig.login()
 
     # insert your desired hashtags here (list)
-    hashtags = ['codnation', 'callofduty', 'cwl', 'twitch', 'blackout', 'killcam', 'codclips', 
-                'blackops', 'freeforall', 'twitchclips', 'blackoutgame', 'xbox', 'ps4',
-                'battleroyale', 'gamingmemes', 'bo4', 'blackops4', 'callofdutyblackops4']
+    hashtags = tags
 
     while True:
         try:
@@ -102,5 +118,5 @@ if __name__ == "__main__":
         except Exception:
             ig.closeBrowser()
             sleep(61)
-            ig = InstagramBot(username, password)
+            ig = InstagramBot(user_id, pword)
 ig.login()
