efcad5b6f0c79f1dd643586649a625c5ae185fe7
Winston <winston@Winstons-MacBook-Pro.local>
2019-05-19 03:10:29 -0700

    forgot user_agent; to eventually include options & profile as well

--- infos.py
+++ infos.py
@@ -3,6 +3,8 @@
 username_box = '//input[@name="username"]'
 # xpath ; password input box
 password_box = '//input[@name="password"]'
+# for the profile necessary to enable mobile view
+user_agent = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16"
 
 """post_to_ig.py"""
 # instagram login url (base)
