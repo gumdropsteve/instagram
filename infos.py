6b6265e18bd75f085fb9642d9b1e71f285f33346
Winston <winston@Winstons-MacBook-Pro.local>
2019-05-20 05:44:03 -0700

    added following & unfollow buttons, maybe some other etc..s as well

--- infos.py
+++ infos.py
@@ -1,3 +1,5 @@
+import pandas as pd 
+
 """paths
 """
 # xpath ; username input box
@@ -10,6 +12,10 @@ save_info_popup = '//*[contains(text(),"Save Info")]'
 like = '//span[@aria-label="Like"]'
 # xpath ; new post button
 new_post_button = '//span[@aria-label="New Post"]'
+# xpath ; following button 
+following_button = '//button[contains(text(),"Following")]'
+# xpath ; unfollow button 
+unfollow_button = '//button[contains(text(),"Unfollow")]'
 
 """urls
 """
@@ -25,6 +31,13 @@ scroll = "window.scrollTo(0, document.body.scrollHeight);"
 # for the profile necessary to enable mobile view
 user_agent = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) \
               AppleWebKit/528.18 (KHTML,like Gecko) Version/4.0 Mobile/7A341 Safari/528.16"
+
+"""data
+"""
+# load accounts & info on followed
+follows_users = pd.read_csv('data/follows_users_ttv_princearthur_20190421_2205.csv')
+# load accounts & info on following
+by_users = pd.read_csv('data/followed_by_users_ttv_princearthru_20190421_1846.csv')
 
 """misc.
 """
