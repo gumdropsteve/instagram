e8c9b7e845285cd8963ecc3742667e7d2fa066b6
Winston <winston@Winstons-MacBook-Pro.local>
2019-05-23 00:27:16 -0700

    *NEW*; ig_not_available; xpath to 2/3 of longer message on Instagram's 404 page

--- infos.py
+++ infos.py
@@ -16,6 +16,8 @@ new_post_button = '//span[@aria-label="New Post"]'
 following_button = '//button[contains(text(),"Following")]'
 # xpath ; unfollow button 
 unfollow_button = '//button[contains(text(),"Unfollow")]'
+# xpath ; instagram 404 page text
+ig_not_available = '//span[contains(text(),"The link you followed may be broken, or the page may have been removed.")]'
 
 """urls
 """
