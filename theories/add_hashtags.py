a7728d51bab06dbf08e25827fdb962d736d4da91
Winston <winston@Winstons-MacBook-Pro.local>
2019-05-25 06:35:01 -0700

    how to run bot from here; dark & lonely; f

---
+++ theories/add_hashtags.py
@@ -0,0 +1,12 @@
+from time import sleep
+from selenium import webdriver
+
+'''
+log in
+    load most recent post (of own account)
+        add comment of hashtags
+            close
+                remove firefox thing from dock (annoying and needed across all)
+'''
+
+
