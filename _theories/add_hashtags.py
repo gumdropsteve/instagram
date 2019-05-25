121b7a704c978ff839a72167d8b22d3c45f48d4b
Winston <winston@Winstons-MacBook-Pro.local>
2019-05-25 07:00:28 -0700

    for adding hashtags to recent post; obvi link to comment

---
+++ _theories/add_hashtags.py
@@ -0,0 +1,10 @@
+from time import sleep
+from selenium import webdriver
+
+'''
+log in
+    load most recent post (of own account)
+        add comment of hashtags
+            close
+                remove firefox thing from dock (annoying and needed across all)
+'''
