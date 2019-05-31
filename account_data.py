28206bdc6ab7f6c361c9941c91c856e695ab8d1e
Winston <winston@Winstons-MacBook-Pro.local>
2019-05-31 08:09:05 -0700

    seems to be running ok; some base testing complete; if 404, metrics = nan,nan,nan,nan --- maybe a better way to do this?, does allow comps on potential_404 to actual 404, though it's possible something else in .getinfo() for that account breaks and is reason for except; may run on first few hundred accounts (or more) before merge

--- account_data.py
+++ account_data.py
@@ -16,16 +16,25 @@ from datetime import datetime
 class Insta_Info_Scraper:
 
     def getinfo(self, url):
+        # specify 
         html = urllib.request.urlopen(url, context=self.ctx).read()
+        # make soup
         soup = BeautifulSoup(html, 'html.parser')
-        data = soup.find_all('meta', attrs={'property': 'og:description'
-                             })
-        # identify and tag text 
+        # identify interest 
+        data = soup.find_all('meta', attrs={'property': 'og:description'})
+        # tag & bag data 
         text = data[0].get('content').split()
         # user name as account calls itself
         user = '%s %s' % (text[-3], text[-2])
         # number of followers
-        followers = int(text[0].replace(',','').replace('k','000'))
+        followers = text[0]
+        # scan for number abbreviatiions
+        if 'k' in followers or 'm' in followers:
+            # keep exactly how is, for study
+            followers = followers
+        # if there are no indicators 
+        else:
+            followers = int(followers.replace(',',''))
         # number of accounts followed
         following = int(text[2].replace(',',''))
         # number of posts
@@ -39,16 +48,19 @@ class Insta_Info_Scraper:
         self.ctx.verify_mode = ssl.CERT_NONE
         # load the record log
         df=pd.read_csv('data/made/verified_accounts_ttvpa_used_to_follow.csv')
-        # # extract urls to each 
-        # urls=[i for i in df.user_profile]
         # que through each account in records
-        for _ in range(len(df[:5])):
+        for _ in range(len(df[:30])):
             # focus whichever account is up
             account = df.loc[_]
             # list out it's datapoints
             datas = [i for i in account]
-            # pull metrics on that account via url
-            metrics = list(self.getinfo(account.user_profile))
+            # will fail if 404 
+            try:
+                # pull metrics on that account via url
+                metrics = list(self.getinfo(account.user_profile))
+            # so for the potential 426 fails
+            except:
+                metrics = ['nan','nan','nan','nan']
             # note the time these metrics were recorded
             metrics.append(datetime.now()) 
             # go through each metric
