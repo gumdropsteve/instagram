5839fe42e119044936d0c2bf9011234863460592
Winston <winston@Winstons-MacBook-Pro.local>
2019-05-31 06:26:28 -0700

    roadblock; how to deal with numbers like 151.4m or 12k? better for long-term if recorded as ints --address issues, avoid problems-- seems could be too difficult for many to understand if was done in '1-line' fashion.. may do if:split style? //; eliminated @ from user name as is already noted w/o @; keeping fields in as-recorded order; somewhat noted; not yet stringed

--- account_data.py
+++ account_data.py
@@ -7,8 +7,10 @@ import urllib.error
 from bs4 import BeautifulSoup
 import ssl
 import json
+from time import sleep
 import pandas as pd
 import csv
+from datetime import datetime
 
 
 class Insta_Info_Scraper:
@@ -18,24 +20,41 @@ class Insta_Info_Scraper:
         soup = BeautifulSoup(html, 'html.parser')
         data = soup.find_all('meta', attrs={'property': 'og:description'
                              })
+        # identify and tag text 
         text = data[0].get('content').split()
-        user = '%s %s %s' % (text[-3], text[-2], text[-1])
-        followers = int(text[0].replace(',',''))
+        # user name as account calls itself
+        user = '%s %s' % (text[-3], text[-2])
+        # number of followers
+        followers = int(text[0].replace(',','').replace('k','000'))
+        # number of accounts followed
         following = int(text[2].replace(',',''))
+        # number of posts
         posts = int(text[4].replace(',',''))
+        # output user/username with #posts, #followers, and #accounts that user is following 
         return user, posts, followers, following
 
     def main(self):
         self.ctx = ssl.create_default_context()
         self.ctx.check_hostname = False
         self.ctx.verify_mode = ssl.CERT_NONE
-
+        # load the record log
         df=pd.read_csv('data/made/verified_accounts_ttvpa_used_to_follow.csv')
-        urls=[i for i in df.user_profile]
-
-        for url in urls[:5]:
-            user = self.getinfo(url)
-
+        # # extract urls to each 
+        # urls=[i for i in df.user_profile]
+        # que through each account in records
+        for _ in range(len(df[:5])):
+            # focus whichever account is up
+            account = df.loc[_]
+            # list out it's datapoints
+            datas = [i for i in account]
+            # pull metrics on that account via url
+            metrics = list(self.getinfo(account.user_profile))
+            # note the time these metrics were recorded
+            metrics.append(datetime.now()) 
+            # go through each metric
+            for metric in metrics:
+                # add it to the account's datapoints 
+                datas.append(metric)
             '''record the transaction
             '''  
             # open up the csv
@@ -43,7 +62,7 @@ class Insta_Info_Scraper:
                 # fit the writer
                 writer = csv.writer(file)
                 # document the transaction
-                writer.writerow(user)
+                writer.writerow(datas)
 
 
 if __name__ == '__main__':
