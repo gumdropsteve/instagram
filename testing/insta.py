04d2fa70e5bbf9f82e82b8a0afe2415d4fb600dc
Winston <winston@Winstons-MacBook-Pro.local>
2019-07-14 03:21:38 -0700

    basic like 10 from default script example

---
+++ testing/insta.py
@@ -0,0 +1,38 @@
+# imports
+from instapy import InstaPy
+from instapy import smart_run
+
+# login credentials
+from user import u, p
+insta_username = u
+insta_password = p
+
+comments = ['Nice shot! @{}',
+            'I love your profile! @{}',
+            'Your feed is an inspiration :thumbsup:',
+            'Just incredible :open_mouth:',
+            'What camera did you use @{}?',
+            'Love your posts @{}',
+            'Looks awesome @{}',
+            'Getting inspired by you @{}',
+            ':raised_hands: Yes!',
+            'I can feel your passion @{} :muscle:']
+
+# get an InstaPy session!
+# set headless_browser=True to run InstaPy in the background
+session = InstaPy(username=insta_username,
+                  password=insta_password,
+                  headless_browser=False)
+
+with smart_run(session):
+    """ Activity flow """		
+    # general settings		
+    session.set_dont_include(["friend1", "friend2", "friend3"])		
+
+    # activity		
+    session.like_by_tags(["codnation"], amount=10)
+
+    # Joining Engagement Pods
+    #   session.set_do_comment(enabled=True, percentage=35)
+    #   session.set_comments(comments)
+    session.join_pods()
