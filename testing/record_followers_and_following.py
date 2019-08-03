97cfe23bbda0a355f4762fa1534fe72284631498
Winston <winston@Winstons-MacBook-Pro.local>
2019-08-03 03:37:54 -0700

    pulls and records followers and following for given account; recorded to csv in t_data/; works on public accounts and private accounts if those private accounts are logged in via the session

--- testing/record_followers_and_following.py
+++ testing/record_followers_and_following.py
@@ -3,73 +3,41 @@ from instapy import InstaPy, relationship_tools, smart_run, unfollow_util
 # login credentials
 from user import u, p
 
-# # set InstaPy session
-# session = InstaPy(username=u, password=p)  # , headless_browser=True
+# set InstaPy session
+session = InstaPy(username=u, password=p, headless_browser=True)
 
-# # start the session
-# with smart_run(session):
-#     # grab followers (list)
-#     followers = session.grab_followers(username="ttv.princearthur", 
-#                                        amount="full", 
-#                                        live_match=True, 
-#                                        store_locally=False)
-#     # grab following (list)
-#     following = session.grab_following(username="ttv.princearthur", 
-#                                        amount="full", 
-#                                        live_match=True, 
-#                                        store_locally=False)
+# start the session
+with smart_run(session):
+    # grab followers (list)
+    followers = session.grab_followers(username="ttv.princearthur", 
+                                       amount="full", 
+                                       live_match=True, 
+                                       store_locally=False)
+    # grab following (list)
+    following = session.grab_following(username="ttv.princearthur", 
+                                       amount="full", 
+                                       live_match=True, 
+                                       store_locally=False)
 
-# # identify accounts our account is following that are followers of our account
-# follow_backers = [account for account in following if account in followers]
-# # identify accounts our account is following that are NOT followers of our account
-# non_follow_backers = [account for account in following if account not in followers]
+# identify accounts our account is following that are followers of our account
+follow_backers = [account for account in following if account in followers]
+# identify accounts our account is following that are NOT followers of our account
+non_follow_backers = [account for account in following if account not in followers]
 
-# # merge for single list of all unique accounts in following/followers
-# unique_accounts = list(set(followers + following))
-
-# # make dataframe of all (unique) accounts
-# df = pd.DataFrame(columns='account', data=unique_accounts)
-
-# bool_followers = []
-# for account in df['account']:
-#     if account in followers:
-#         bool_followers.append(1)
-#     else:
-#         bool_followers.append(0)
-
-# bool_following = []
-# for account in df['account']:
-#     if account in following:
-#         bool_following.append(1)
-#     else:
-#         bool_following.append(0)
-
-# # add bool columns to dataframe
-# df['follower'] = bool_followers
-# df['following'] = bool_following
-
-# # record dataframe as csv
-# df.to_csv(path_or_buf='t_data/followers_and_following.csv')
-
-
-"""testing area
-"""
-
-followers = ['ricky', 'james', 'moses', 'winston', 'zack', 'joe']
-following = ['ricky', 'ricardo', 'winston', 'zack', 'tommy']
 # merge for single list of all unique accounts in following/followers
 unique_accounts = list(set(followers + following))
 
 # make dataframe of all (unique) accounts
-df = pd.DataFrame(data=unique_accounts, columns=['account'])
+df = pd.DataFrame(columns=['account'], data=unique_accounts)
 
-# bool followers & following (1 == True)
+# make bool list of followers (1 == True)
 bool_followers = []
 for account in df['account']:
     if account in followers:
         bool_followers.append(1)
     else:
         bool_followers.append(0)
+# make bool list of following (1 == True)
 bool_following = []
 for account in df['account']:
     if account in following:
@@ -82,4 +50,4 @@ df['follower'] = bool_followers
 df['following'] = bool_following
 
 # record dataframe as csv
-df.to_csv(path_or_buf='t_data/followers_and_following.csv')
+df.to_csv(path_or_buf='t_data/followers_and_following.csv', index=False)
