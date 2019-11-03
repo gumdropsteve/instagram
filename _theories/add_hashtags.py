e772e0aba09a335e3588daf0ce8d38d31711e279
gumdropsteve <warobson@gmail.com>
2019-11-03 05:46:14 -0800

    base psudo -- this should be used for comments

--- _theories/add_hashtags.py
+++ _theories/add_hashtags.py
@@ -8,3 +8,13 @@ log in
             close
                 remove firefox thing from dock (annoying and needed across all)
 '''
+
+def add_hashtags(self, post, hashtags):
+    # pull up post 
+    self.driver.get(post)
+    # locate & click comment button
+    # self.driver.find_element_by_....click()
+    # write out hashtags
+    # self.driver.find_element_by_....send_keys(post)
+    # find button & submit comment
+    # self.driver.find_element_by_....click()
